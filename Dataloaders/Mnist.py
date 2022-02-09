5b21fa28da001c7bdab93c451b07dab4698b5b5a
Martin Gullbrandson <45486484+MartinHex@users.noreply.github.com>
2022-02-09 14:27:38 +0100

    Add federated dataloader for mnist, currently only support iid data between clients

---
+++ Dataloaders/Mnist.py
@@ -0,0 +1,69 @@
+from Dataloaders.federated_dataloader import FederatedDataLoader
+import torchvision
+import numpy as np
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+class Mnist(FederatedDataLoader):
+    def __init__(self, number_of_clients,  download = True):
+        self.transform = torchvision.transforms.Compose(
+            [torchvision.transforms.ToTensor()
+            ])
+
+        self.number_of_clients = number_of_clients
+
+        self.trainset = torchvision.datasets.MNIST(root= './data', train = True, download = download, transform = self.transform)
+        self.testset = torchvision.datasets.MNIST(root = './data', train = False, download = download, transform = self.transform)
+
+        assert len(self.trainset) % self.number_of_clients == 0, "Number of clients must be evenly devicible with the length of the dataset, length of the dataset is {}".format(len(self.trainset))
+
+        self.split_trainset = self._create_trainset()
+
+    def get_training_dataloaders(self, batch_size, shuffle = True):
+        dataloaders = []
+        for client in self.split_trainset:
+            dataloaders.append(DataLoader(ImageDataset(client), batch_size = batch_size, shuffle = shuffle))
+        return dataloaders
+
+    def get_test_dataloader(self):
+        return None
+
+    def get_training_raw_data(self):
+        return self.split_trainset
+
+    def get_test_raw_data(self):
+        return None
+
+    # Split the dataset into clients, if we want to make the dataset non-iid this is where we'd do that.
+    def _create_trainset(self):
+        randomized_index = np.random.choice(len(self.trainset), len(self.trainset), replace = False)
+        client_list = [[] for i in range(self.number_of_clients)]
+        for i in range(len(self.trainset)):
+            client_index = int( (i * self.number_of_clients) / len(self.trainset))
+            client_list[client_index].append((self.trainset[randomized_index[i]][0], self.trainset[randomized_index[i]][1]))
+        return client_list
+
+class ImageDataset(Dataset):
+    """Constructor
+
+    Args:
+        data: list of data for the dataset.
+    """
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, index):
+        """Get sample by index
+
+        Args:
+            index (int)
+
+        Returns:
+             The index'th sample (Tensor, int)
+        """
+        tensor, label = self.data[index]
+        return tensor, label
+
+    def __len__(self):
+        """Total number of samples"""
+        return len(self.data)
