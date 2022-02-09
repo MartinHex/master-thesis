1358b17d407cd0dd83533b604c562385d3b911da
Martin Gullbrandson <45486484+MartinHex@users.noreply.github.com>
2022-02-09 11:40:26 +0100

    Refactor the Dirichlet CIFAR 100 dataloader to comply with the abstract federated dataloader class

--- Dataloaders/DirichletCifar100.py
+++ Dataloaders/DirichletCifar100.py
@@ -2,18 +2,21 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from Dataloaders.federated_dataloader import FederatedDataLoader
 
-class DirichletCifar100():
+class DirichletCifar100(FederatedDataLoader):
     '''
         Use a heirichical Dirichlet sampling to sample natural clients of the CIFAR-100 dataset, as proposed
         by Reddi et al. (https://arxiv.org/pdf/2003.00295.pdf)
     '''
-    def __init__(self, number_of_clients, alpha = 0.1, beta = 10):
+    def __init__(self, number_of_clients, alpha = 0.1, beta = 10, download = True):
         self.transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
             ])
-        
+
         self.map_coarse_to_fine_label = {
             0: [4, 30, 55, 72, 95], # 0
             1: [1, 32, 67, 73, 91], # 1
@@ -36,24 +39,46 @@ class DirichletCifar100():
             18 : [8, 13, 48, 58, 90], # 18
             19 : [41, 69, 81, 85, 89], # 19
         }
-        
+
         self.number_of_clients = number_of_clients
         self.alpha = alpha
         self.beta = beta
 
-        self.trainset = torchvision.datasets.CIFAR100(root= './data', train = True, download = False, transform = self.transform)
-        self.testset = torchvision.datasets.CIFAR100(root = './data', train = False, download = False, transform = self.transform)
-        
-        
-    def get_trainset(self):
+        self.trainset = torchvision.datasets.CIFAR100(root= './data', train = True, download = download, transform = self.transform)
+        self.testset = torchvision.datasets.CIFAR100(root = './data', train = False, download = download, transform = self.transform)
+
+        self.split_trainset = self._create_trainset()
+
+    def get_training_raw_data(self):
+        return self.split_trainset
+
+    def get_test_raw_data(self):
+        return None
+
+    def get_training_dataloaders(self, batch_size, shuffle = True,  label = 'fine'):
+        datasets = []
+        for client in self.split_trainset:
+            datasets.append(ImageDataset(client, label))
+        dataloaders = []
+        for dataset in datasets:
+            dataloaders.append(DataLoader(dataset, batch_size = batch_size, shuffle = shuffle))
+
+        return dataloaders
+
+    def get_test_dataloader(self):
+        return None
+
+
+
+    def _create_trainset(self):
         class_dict = self._create_class_dict()
-        
+
         number_of_samples = int(len(self.trainset) / self.number_of_clients) # N
         client_sets = []
-                                
+
         available_coarse_label = [x for x in range(20)] # G(r)
         available_fine_label = self.map_coarse_to_fine_label.copy() #G(c)
-        
+
         clients = []
 
         for client in range(self.number_of_clients):
@@ -102,8 +127,8 @@ class DirichletCifar100():
 
             clients.append(client_samples)
         return clients
-        
-        
+
+
     def _create_class_dict(self):
         # Map all the images into a dict with it's targets as key.
         class_dict = dict()
@@ -115,11 +140,45 @@ class DirichletCifar100():
 
         # Make the index list randomly shuffled --> unifrom random distributed
         for key, value in class_dict.items():
-            np.random.shuffle(value) 
+            np.random.shuffle(value)
         return class_dict
-    
+
     def _renormalize(self, theta):
         s = 0
         for l in theta:
             s += l
         map(lambda x: x/s, theta)
+
+class ImageDataset(Dataset):
+
+    #def __init__(self, root, transform, dog_label=1, cat_label=0):
+    def __init__(self, data, label):
+        """Constructor
+
+        Args:
+            root (Path/str): Filepath to the data root, e.g. './small_train'
+            transform (Compose): A composition of image transforms, see below.
+        """
+        self.data = data
+        self.label = label
+
+    def __getitem__(self, index):
+        """Get sample by index
+
+        Args:
+            index (int)
+
+        Returns:
+             The index'th sample (Tensor, int)
+        """
+        tensor, coarse_label, fine_label = self.data[index]
+        if self.label == 'fine':
+            return tensor, fine_label
+        elif self.label == 'coarse':
+            return tensor, coarse_label
+        else:
+            raise Exception('Wrong type of label: CIFAR 100 only support fine or coase labels.')
+
+    def __len__(self):
+        """Total number of samples"""
+        return len(self.data)
