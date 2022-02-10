d04cea5d70b956d7dd2a9bc3508a25d5006642f2
Martin Gullbrandson <45486484+MartinHex@users.noreply.github.com>
2022-02-10 14:04:31 +0100

    Add ability to have multiple callbaks with example

--- Algorithms/ABCAlgorithm.py
+++ Algorithms/ABCAlgorithm.py
@@ -2,17 +2,18 @@ from abc import ABC,abstractmethod
 
 class ABCAlgorithm(ABC):
 
-    def __init__(self,n_clients,dataloader,Architecture,callback):
+    def __init__(self,n_clients,dataloader,Architecture,callbacks):
         self.n_clients=n_clients
         self.dataloader=dataloader
         self.Architecture = Architecture
-        self.callback = callback
+        self.callbacks = callbacks
 
     def run(self,iterations):
         for round in range(iterations):
             for client in self.clients:
                 loss = client.train()
         self.server.aggregate(self.clients)
-        if(self.callback != None):
-            self.callback(self)
+        if(self.callbacks != None):
+            for callback in self.callbacks:
+                callback(self)
         self.server.push_weights(self.clients)
--- exampleAlgorithm.py
+++ exampleAlgorithm.py
@@ -8,10 +8,19 @@ batch_size = 16
 mnist = Mnist(number_of_clients)
 
 test_data = mnist.get_test_dataloader(batch_size)
-def callback(self):
-    test_loss = self.server.model.evaluate(test_data)
-    print("Current Server val loss: %.5f"%test_loss)
+def server_loss(self):
+    test_loss = self.server.evaluate(test_data)
+    print("Current Server val loss: {:.5f}".format(test_loss))
 
-alg = FedAvg(dataloader=mnist,Architecture=MNIST_Architecture,callback=callback,n_clients=5)
+def client_loss(self):
+    for i, client in enumerate(self.clients):
+        client_loss = client.evaluate(test_data)
+        print("Client {}: val loss: {:.5f}".format(i, client_loss))
+
+callbacks = [
+    client_loss,
+    server_loss,
+]
+alg = FedAvg(dataloader=mnist, Architecture=MNIST_Architecture, callbacks = callbacks, n_clients=5)
 
 alg.run(1)
