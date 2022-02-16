cec5789f030bb80b870336d691742e7a20339e83
Martin Gullbrandson <45486484+MartinHex@users.noreply.github.com>
2022-02-16 15:40:55 +0100

    Add first implementation of Emnist-62 dataloader and Emnist model
    The EMNIST-62 dataloader currently use the JSON files produced by the leaf repository produced by S. Caldas et al. It require some setup of the data files, explanation in a README will be done later.

---
+++ Dataloaders/Emnist62.py
@@ -0,0 +1,121 @@
+from Dataloaders.federated_dataloader import FederatedDataLoader
+#import torchvision
+#import numpy as np
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import os
+import json
+from collections import defaultdict
+from tqdm import tqdm
+import numpy as np
+import torch
+
+class EMNIST(FederatedDataLoader):
+    """Federated wrapper class for the Torchvision EMNIST-62 dataset
+
+    Assumes:
+        - That the Federated EMNIST-62 (FEMNIST) dataset is present in ../data/femnist.
+        - The data is split by using the method presented by Caldas et al. (https://github.com/TalwalkarLab/leaf)
+    """
+    def __init__(self, number_of_clients, test_size = 0.3, data_path = None, test_path = None):
+        if data_path:
+            self.data_path = data_path
+        else:
+            self.data_path = os.path.join('..', 'data', 'femnist' 'all_data')
+
+        self.number_of_clients = number_of_clients
+        self.test_size = int(self.number_of_clients * test_size)
+
+        assert self.test_size > 0, "The number of test clients is less than 1"
+
+        self.testset = []
+        self.trainset = []
+
+        self._test_train_split()
+        self.unified_testset = [item for sublist in self.testset for item in sublist]
+
+    def get_training_dataloaders(self, batch_size, shuffle = True):
+        dataloaders = []
+        for client in self.trainset:
+            dataloaders.append(DataLoader(ImageDataset(client), batch_size = batch_size, shuffle = shuffle))
+        return dataloaders
+
+    def get_test_dataloader(self, batch_size):
+        return DataLoader(ImageDataset(self.unified_testset), batch_size = batch_size, shuffle = False)
+
+    def get_training_raw_data(self):
+        return self.trainset
+
+    def get_test_raw_data(self):
+        return self.unified_testset
+
+    def _test_train_split(self):
+        all_clients = self._get_all_clients()
+        clients = list(all_clients.keys())
+
+        train_index = np.random.choice(range(len(clients)), size = self.number_of_clients, replace = False)
+        train_clients = [clients[i] for i in train_index]#clients[train_index]
+
+        test_index = np.random.choice(list(set(range(len(clients))) - set(train_index)), size = self.test_size, replace = False)
+        test_clients = [clients[i] for i in test_index] #clients[test_index]
+
+        assert len(set(train_clients) | set(test_clients)) == (len(set(train_clients)) + len(set(test_clients))), "Clients appear in both training and test set"
+
+        print('Loading training clients...')
+        for client in tqdm(train_clients):
+            file = all_clients[client]
+            self.trainset.append(self._read_client(client, file))
+            print('Loading test clients...')
+        for client in tqdm(test_clients):
+            file = all_clients[client]
+            self.testset.append(self._read_client(client, file))
+
+    def _read_client(self, client, file):
+        file_path = os.path.join(self.data_path, file)
+        with open(file_path, 'r') as inf:
+            data = json.load(inf)
+
+        client_data = list(zip(data['user_data'][client]['x'], data['user_data'][client]['y']))
+        for i, (x, y) in enumerate(client_data):
+            client_data[i] = (torch.reshape(torch.Tensor(x), (28,28)), y)
+
+        return client_data
+
+    def _get_all_clients(self):
+        all_clients = dict()
+        files = os.listdir(self.data_path)
+        files = [f for f in files if f.endswith('.json')]
+        print('Collecting all available authors...')
+        for f in tqdm(files):
+            file_path = os.path.join(self.data_path,f)
+            with open(file_path, 'r') as inf:
+                cdata = json.load(inf)
+            file_clients = cdata['users']
+            for client in file_clients:
+                all_clients[client] = f
+        return all_clients
+
+class ImageDataset(Dataset):
+    """Constructor
+
+    Args:
+        data: list of data for the dataset.
+    """
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, index):
+        """Get sample by index
+
+        Args:
+            index (int)
+
+        Returns:
+             The index'th sample (Tensor, int)
+        """
+        tensor, label = self.data[index]
+        return tensor, label
+
+    def __len__(self):
+        """Total number of samples"""
+        return len(self.data)
---
+++ Models/EMNIST_Model.py
@@ -0,0 +1,102 @@
+from torch import nn
+from torch.autograd import Variable
+import copy
+
+class EMNIST_Model(nn.Module):
+    """CNN model indended to be used on the EMNIST-62 dataset
+
+    This model is a replica of the model used by S. Reddit et al. (https://arxiv.org/pdf/2003.00295.pdf) and by M. AlShedivat et al. (https://arxiv.org/pdf/2010.05273.pdf)
+    """
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels = 1,
+            out_channels = 32,
+            kernel_size = 3,
+            stride = 1,
+            padding = 0,
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels = 32,
+            out_channels = 64,
+            kernel_size = 3,
+            stride = 1,
+            padding = 0,
+        )
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(
+            kernel_size = 2,
+            stride = 2,
+            padding = 0,
+        )
+        self.dropout1 = nn.Dropout(p = 0.25)
+        self.dropout2 = nn.Dropout(p = 0.5)
+        self.dense1 = nn.Linear(9216, 128)
+        self.dense2 = nn.Linear(128, 62)
+        self.softmax = nn.Softmax(dim = 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.dropout1(x)
+
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)
+
+        x = self.dense1(x)
+        x = self.dropout2(x)
+        x = self.dense2(x)
+        output = self.softmax(x)
+        return output, x    # return x for visualization
+
+    def evaluate(self,dataloader,loss_func):
+        """Evaluates the model on a given loss function.
+
+            Arguments:
+                - dataloader: Dataloader with data to evaluate on.
+                - loss_func: Loss function to use for evaluation.
+        """
+        self.eval() # prep model for evaluation
+        server_loss = 0
+        for data, target in dataloader:
+            # forward pass: compute predicted outputs by passing inputs to the model
+            output = self(data)
+            # calculate the loss
+            loss = loss_func(output[0], target)
+            # update running validation loss
+            server_loss += loss.item()/data.size(0)
+        server_loss = server_loss/len(dataloader)
+        return server_loss
+
+    def train_model(self, dataloader,optimizer,loss_func=nn.CrossEntropyLoss(),epochs = 1):
+        """Trains the models a set number of epochs.
+
+            Arguments:
+                - dataloader: The training data in the form of a Pytorch Dataloader.
+                - optimizer: The PyTorch implementation of a optimizer.
+                - loss_func: The PyTorch implementation of a loss function (Default: Categorical Cross Entropy).
+                - epochs: The number of epochs to run (Default: 1)
+        """
+        self.train()
+        for epoch in range(epochs):
+            for i, (input_data, labels) in enumerate(dataloader):
+                # gives batch data, normalize x when iterate train_loader
+                b_x = Variable(input_data)   # batch x
+                b_y = Variable(labels)   # batch y
+                output = self(b_x)[0]
+                loss = loss_func(output, b_y)
+                # clear gradients for this training step
+                optimizer.zero_grad()
+                # backpropagation, compute gradients
+                loss.backward()
+                # apply gradients
+                optimizer.step()
+        return loss.item()
+
+    def get_weights(self):
+        return  copy.deepcopy(self.state_dict())
+
+    def set_weights(self, model_state):
+        self.load_state_dict(copy.deepcopy(model_state))
