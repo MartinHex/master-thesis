d4e8b72ada1658b7151b51e2c4def768b09ab62b
Oscar Johansson <o.n.johansson@gmail.com>
2022-03-08 17:22:35 +0100

    Minor FedKP adjustment.

--- Algorithms/FedKp.py
+++ Algorithms/FedKp.py
@@ -6,5 +6,5 @@ class FedKp(Algorithm):
     def __init__(self,dataloader,Model,callbacks=None, save_callbacks = False,batch_size=16,clients_per_round=None):
         client_dataloaders = dataloader.get_training_dataloaders(batch_size)
         clients = [SGDClient(Model(), loader) for loader in client_dataloaders]
-        server = FedKPServer(Model())
+        server = FedKpServer(Model())
         super().__init__(server,clients, callbacks, save_callbacks,clients_per_round=clients_per_round)
