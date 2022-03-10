de6d258254284448f855d9ca4f7e632f1f63a072
root <root@Scaleout-Probabilistic.edgelab.network>
2022-03-10 11:32:07 +0100

    Fixed minor bugs, FedPA is working now

--- Clients/FedPaClient.py
+++ Clients/FedPaClient.py
@@ -34,12 +34,8 @@ class FedPaClient(Base_Client):
         sum_factors = []
 
         for epoch in range(epochs):
-            sample_weight = self._produce_iasg_sample(device)
-
-            # If new IASG sample we update moving delta.
-            #t += 1
             t = epoch + 1
-            for key in sample_weight.keys():
+            sample_weight = self._produce_iasg_sample(device)
 
             u_t = sample_weight.sub(current_average)
             sum = torch.zeros(self.model_size).to(device)
@@ -47,26 +43,26 @@ class FedPaClient(Base_Client):
                 gamma_k = (self.beta * k) / (k + 1) #Index different due to python start @ 0, Equation 17.
                 nominator = (gamma_k * v[k].matmul(u_t))
                 if k == t - 2:
-                    denominator = 1 + gamma_k * v[k].(u_key)
-                    sum_factors.append(gamma_k/denominator)))
+                    denominator = 1 + gamma_k * v[k].matmul(u_t)
+                    sum_factors.append(gamma_k/denominator)
                 factor = sum_factors[k]
-                sum = sum.add(v[k].mul(sum_factors))
+                sum = sum.add(v[k].mul(sum_factors[k]))
             v.append(u_t.sub(sum)) #Equation 28
-            current_average = sample_weight.add(current_average, alpha = l).div(l + 1)
+            current_average = sample_weight.add(current_average, alpha = t).div(t+1)
             gamma_t = (self.beta * (t - 1)) / t #Equation 17.
-            nominator = gamma_t * (t * u_t.matmul(current_delta) - u_key.matmul(v[t]))
+            nominator = gamma_t * (t * u_t.matmul(current_delta) - u_t.matmul(v[t]))
             denominator = 1 + gamma_t * u_t.matmul(v[t])
             current_delta = current_delta.sub((1 + nominator / denominator).mul(v[t]).div(t + 1)) #Equation 23
             current_average = sample_weight.add(current_average, alpha = t).div(t + 1)
 
-        new_weights =  initial_weights.sub(current_delta.cpu() / self.shrinkage) #Gradient from Equation 2
+        new_weights =  initial_weights.sub(current_delta / self.shrinkage).cpu() #Gradient from Equation 2
         new_weights = self._array_to_model_weight(new_weights)
         return new_weights
 
     def _produce_iasg_sample(self, device):
         sample_weight = dict()
-        for i, weights in enumerate(self.model.iter_train_model(self.dataloader,self.optimizer, device = device)):
-            for key in weight.keys():
+        for i, weight in enumerate(self.model.iter_train_model(self.dataloader,self.optimizer, device = device)):
+            for key in weight:
                 if (i == 0):
                     sample_weight[key] = weight[key]
                 else:
