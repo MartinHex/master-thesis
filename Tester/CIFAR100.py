b6b28525a9aba67e1c7aeb0439ebfac33093fc2e
root <root@Scaleout-Probabilistic.edgelab.network>
2022-03-10 13:54:55 +0100

    Fix base CIFAR100 test

--- Tester/CIFAR100.py
+++ Tester/CIFAR100.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('.')
 #Import models
 from Models.CIFAR_Model import CIFAR_Model as Model
 from Dataloaders.DirichletCifar100 import DirichletCifar100 as Dataloader
@@ -12,68 +14,74 @@ from Algorithms.Algorithm import Algorithm
 from Clients.FedPaClient import FedPaClient
 from Servers.FedAvgServer import FedAvgServer
 from Clients.SGDClient import SGDClient
-from Servers.FedBeServer import FedBeServer
 # Additional imports
 from Models.Callbacks.Callbacks import Callbacks
 import matplotlib.pyplot as plt
 import torch
 
 
-number_of_clients = 100
+number_of_clients = 500
+clients_per_round = 20
 batch_size = 16
-alpha = input("alpha=")
-beta = input("beta=")
+alpha = 0.1
+beta = 100
 dataloader = Dataloader(number_of_clients,alpha=alpha,beta=beta)
 test_data = dataloader.get_test_dataloader(batch_size)
 device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
 
 # Create callback functions that are run at the end of every round
-cbs_pa = Callbacks(test_data, device = device, verbose = False)
-cbs_be = Callbacks(test_data, device = device, verbose = False)
-cbs_ag = Callbacks(test_data, device = device, verbose = False)
-cbs_kp = Callbacks(test_data, device = device, verbose = False)
+print('Creating Callbacks')
+cbs_pa = Callbacks(test_data, device = device, verbose = True)
+cbs_be = Callbacks(test_data, device = device, verbose = True)
+cbs_ag = Callbacks(test_data, device = device, verbose = True)
+cbs_kp = Callbacks(test_data, device = device, verbose = True)
 callbacks_pa = [
-    ('client_loss', cbs_pa.client_loss),
     ('server_pa_loss', cbs_pa.server_loss),
-    ('client_accuracy', cbs_pa.client_accuracy),
     ('server_pa_accuracy', cbs_pa.server_accuracy),
 ]
 callbacks_be = [
-    ('client_loss', cbs_be.client_loss),
     ('server_be_loss', cbs_be.server_loss),
-    ('client_accuracy', cbs_be.client_accuracy),
     ('server_be_accuracy', cbs_be.server_accuracy),
 ]
 callbacks_ag = [
-    ('client_loss', cbs_ag.client_loss),
     ('server_ag_loss', cbs_ag.server_loss),
-    ('client_accuracy', cbs_ag.client_accuracy),
     ('server_ag_accuracy', cbs_ag.server_accuracy),
 ]
 callbacks_kp = [
-    ('client_loss', cbs_kp.client_loss),
     ('server_kp_loss', cbs_kp.server_loss),
-    ('client_accuracy', cbs_kp.client_accuracy),
     ('server_kp_accuracy', cbs_kp.server_accuracy),
 ]
 
-# Set parameters to replicate paper results
-# FedPA
-fedpa_clients = [FedPaClient( Model(), dl, learning_rate = 0.01, burn_in =  400,
-                                K = 10, shrinkage = 0.01, mcmc_samples = 1100)
-                                for dl in dataloader.get_training_dataloaders()]
-fedpa_server = FedAvgServer(Model())
-fedpa = Algorithm(fedpa_server,fedpa_clients, callbacks = callbacks_pa)
-
-# FedBe uses cifar100 as default arguments.
 # Initiate algorithms with same parameters as in papers.
-alghs = [FedAg(dataloader=dataloader, Model=Model, callbacks = callbacks_ag, save_callbacks = True),
-        FedBe(dataloader=dataloader, Model=Model, callbacks = callbacks_be, save_callbacks = True),
-        fedpa,
-        FedKp(dataloader=dataloader, Model=Model, callbacks = callbacks_kp, save_callbacks = True)
+# Set parameters to replicate paper results
+print('Creating FedAG')
+fedag = FedAg(dataloader=dataloader, Model=Model, callbacks = callbacks_ag,
+    clients_per_round = clients_per_round, save_callbacks = True, batch_size = batch_size)
+
+print('Creating FedBE')
+fedbe = FedBe(dataloader=dataloader, Model=Model, callbacks = callbacks_be,
+    clients_per_round = clients_per_round, save_callbacks = True, batch_size = batch_size)
+
+print('Creating FedPA')
+fedpa = FedPa(dataloader=dataloader, Model=Model, callbacks = callbacks_pa,
+    clients_per_round = clients_per_round, save_callbacks = True,client_lr = 0.01,
+    burn_in =  0.8, shrinkage = 0.01,batch_size=batch_size)
+
+print('Creating FedKP')
+fedkp = FedKp(dataloader=dataloader, Model=Model, callbacks = callbacks_kp,
+    clients_per_round = clients_per_round, save_callbacks = True,
+    cov_adj=False,batch_size = batch_size)
+
+alghs = [
+    fedbe,
+    fedag,
+    fedpa,
+    fedkp
 ]
 
+print('Initializing Clients')
 alghs[0].server.push_weights([alg.server for alg in alghs[1:]])
-iterations = 30
+iterations = 600
+print('Running Algorithms')
 for alg in alghs:
-    alg.run(iterations, epochs = 10)
+    alg.run(iterations, epochs = 5, device = device)
