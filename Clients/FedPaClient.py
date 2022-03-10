f30e122bc03fd63417a7e1db7c72a465dc9a8331
Oscar Johansson <o.n.johansson@gmail.com>
2022-03-10 11:10:57 +0100

    Optimized fedpa performance.

--- Clients/FedPaClient.py
+++ Clients/FedPaClient.py
@@ -10,9 +10,16 @@ class FedPaClient(Base_Client):
         self.optimizer = optim.SGD(self.model.parameters(), lr = learning_rate,
                                 momentum=momentum,weight_decay=decay,dampening=dampening)
         self.dataloader = dataloader
-        #self.burn_in = burn_in
-        #self.K = K
-        #self.mcmc_samples = mcmc_samples
+
+        # Initiate model architecture for flattening
+        w = model.get_weights()
+        self.layers = list(w)
+        # Set tensorlengths for future reconstruction of flattening.
+        self.layer_size = [len(w[k].flatten()) for k in w]
+        self.model_size = sum(self.layer_size)
+        self.layer_shapes = [w[k].size() for k in w]
+
+        # FedPa parameters
         self.beta = shrinkage
         self.shrinkage = shrinkage
 
@@ -20,14 +27,12 @@ class FedPaClient(Base_Client):
         # Produce the desired gradient online and at any-time as described in appendix C by Al-Shedivat et al.
         # (https://arxiv.org/pdf/2010.05273.pdf)
         initial_weights = self.model.get_weights()
-        current_delta = dict()
-        current_average = dict()
-        v = defaultdict(lambda: [])
-        denominators = defaultdict(lambda: [])
-        for key in initial_weights.keys():
-            current_average[key] = initial_weights[key].to(device)
-            current_delta[key] = initial_weights[key].sub(current_average[key])
-            v[key].append(torch.empty(initial_weights[key].shape).to(device))
+        initial_weights = self._model_weight_to_array(initial_weights).to(device)
+        current_delta = torch.clone(initial_weights)
+        current_average = torch.zeros(self.model_size).to(device)
+        v = [torch.zeros(self.model_size).to(device)]
+        sum_factors = []
+
         for epoch in range(epochs):
             sample_weight = self._produce_iasg_sample(device)
 
@@ -35,25 +40,27 @@ class FedPaClient(Base_Client):
             #t += 1
             t = epoch + 1
             for key in sample_weight.keys():
-                u_key = sample_weight[key].sub(current_average[key])
-                sum = torch.zeros(sample_weight[key].shape).to(device)
-                for k in range(t - 1):
-                    gamma_k = (self.beta * k) / (k + 1) #Index different due to python start @ 0, Equation 17.
-                    nominator = (gamma_k * torch.flatten(v[key][k]).matmul(torch.flatten(u_key)).item())
-                    if k == t - 2:
-                        denominators[key].append(1 + gamma_k * torch.flatten(v[key][k]).matmul(torch.flatten(u_key)).item())
-                    denominator = denominators[key][k]
-                    sum = sum.add(v[key][k].mul(nominator / denominator))
-                v[key].append(u_key.sub(sum)) #Equation 28
-                current_average[key] = sample_weight[key].add(current_average[key], alpha = l).div(l + 1)
-                gamma_t = (self.beta * (t - 1)) / t #Equation 17.
-                nominator = gamma_t * (t * torch.flatten(u_key).matmul(torch.flatten(current_delta[key])) - torch.flatten(u_key).matmul(torch.flatten(v[key][t])))
-                denominator = 1 + gamma_t * torch.flatten(u_key).matmul(torch.flatten(v[key][t]))
-                current_delta[key] = current_delta[key].sub((1 + nominator / denominator).mul(v[key][t]).div(t + 1)) #Equation 23
-                current_average[key] = sample_weight[key].add(current_average[key], alpha = t).div(t + 1)
-        new_weights = dict()
-        for key in initial_weights.keys():
-            new_weights[key] = initial_weights[key].sub(current_delta[key].cpu() / self.shrinkage) #Gradient from Equation 24
+
+            u_t = sample_weight.sub(current_average)
+            sum = torch.zeros(self.model_size).to(device)
+            for k in range(t - 1):
+                gamma_k = (self.beta * k) / (k + 1) #Index different due to python start @ 0, Equation 17.
+                nominator = (gamma_k * v[k].matmul(u_t))
+                if k == t - 2:
+                    denominator = 1 + gamma_k * v[k].(u_key)
+                    sum_factors.append(gamma_k/denominator)))
+                factor = sum_factors[k]
+                sum = sum.add(v[k].mul(sum_factors))
+            v.append(u_t.sub(sum)) #Equation 28
+            current_average = sample_weight.add(current_average, alpha = l).div(l + 1)
+            gamma_t = (self.beta * (t - 1)) / t #Equation 17.
+            nominator = gamma_t * (t * u_t.matmul(current_delta) - u_key.matmul(v[t]))
+            denominator = 1 + gamma_t * u_t.matmul(v[t])
+            current_delta = current_delta.sub((1 + nominator / denominator).mul(v[t]).div(t + 1)) #Equation 23
+            current_average = sample_weight.add(current_average, alpha = t).div(t + 1)
+
+        new_weights =  initial_weights.sub(current_delta.cpu() / self.shrinkage) #Gradient from Equation 2
+        new_weights = self._array_to_model_weight(new_weights)
         return new_weights
 
     def _produce_iasg_sample(self, device):
@@ -64,4 +71,14 @@ class FedPaClient(Base_Client):
                     sample_weight[key] = weight[key]
                 else:
                     sample_weight[key] = weight[key].add(sample_weight[key], alpha = i).div((i + 1))
-        return sample_weight
+        return self._model_weight_to_array(sample_weight).to(device)
+
+
+    def _model_weight_to_array(self,w):
+        return torch.cat([w[k].flatten() for k in w]).detach()
+
+    def _array_to_model_weight(self,a):
+        a_tens = torch.split(a,self.layer_size)
+        model_w = {k:torch.reshape(a_tens[i],self.layer_shapes[i])
+                    for i,k in enumerate(self.layers )}
+        return model_w
