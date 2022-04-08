6002dabac15a3c735365700b69ade9f9aa559b9f
Martin Gullbrandson <45486484+MartinHex@users.noreply.github.com>
2022-04-08 09:22:15 +0200

    Rework FedPA client to emulate the code implemented in tff

--- Clients/FedPaClient.py
+++ Clients/FedPaClient.py
@@ -30,44 +30,102 @@ class FedPaClient(Base_Client):
 
     def train(self,epochs=1, device = None):
         # Produce the desired gradient online and at any-time as described in appendix C by Al-Shedivat et al.
-        # (https://arxiv.org/pdf/2010.05273.pdf) mostly copied from:
-        # https://github.com/alshedivat/fedpa/blob/master/federated/inference/local.py#L170-L226
-        initial_weights = self.model.get_weights()
-        initial_weights = self._model_weight_to_array(initial_weights).to(device)
+        # (https://arxiv.org/pdf/2010.05273.pdf) mostly translated from:
+        # https://github.com/google-research/federated/tree/master/posterior_averaging
+        x_0 = self.model.get_weights()
         num_samples = epochs
         samples = []
         for epoch in range(epochs):
             samples.append(self._produce_iasg_sample(device))
-        rho = self.shrinkage
-        dp = defaultdict(list)
-        samples_ra = samples[0]
-        delta = initial_weights - samples_ra
-        for t, s in enumerate(samples[1:], 2):
-            u = v = s - samples_ra
-            # Compute v_{t-1,t} (solution of `sigma_{t-1} x = u_t`).
-            for k, (v_k, dot_uk_vk) in enumerate(zip(dp["v"], dp["dot_u_v"]), 2):
-                gamma_k = rho * (k - 1) / k
-                v = v - gamma_k * torch.dot(v_k, u) / (1 + gamma_k * dot_uk_vk) * v_k
-            # Compute `dot(u_t, v_t)` and `dot(u_t, delta_t)`.
-            dot_u_v = torch.dot(u, v)
-            dot_u_d = torch.dot(u, delta)
-            # Compute delta.
-            gamma = rho * (t - 1) / t
-            c = gamma * (t * dot_u_d - dot_u_v) / (1 + gamma * dot_u_v)
-            # Calculate new delta and replace nans
-            delta -= (1 + c) * v / t
-            # Update the DP state.
-            dp["v"].append(v)
-            dp["dot_u_v"].append(dot_u_v)
-            # Update running mean of the samples.
-            samples_ra = ((t - 1) * samples_ra + s) / t
-        final_delta = delta * (1 + (num_samples - 1) * rho)
-        # Avoid gradient clipping
-        norm = torch.norm(final_delta)
-        if(norm>1):final_delta=final_delta/norm
-        new_weights = initial_weights.sub(final_delta)
-        new_weights = self._array_to_model_weight(new_weights)
-        self.set_weights(new_weights)
+        with torch.no_grad():
+            rho = self.shrinkage
+
+            # First sample
+            weights_sample_mean = samples[0]
+            weights_delta_tilde = dict()
+            for key in weights_sample_mean:
+                weights_delta_tilde[key] = x_0[key] - weights_sample_mean[key]
+
+            # Second Sample
+            un_flat = dict()
+            vn_flat = dict()
+            for key in samples[1]:
+                un_flat[key] = (samples[1][key].clone() - weights_sample_mean[key].clone()).flatten()
+                vn_flat[key] = (samples[1][key].clone() - weights_sample_mean[key].clone()).flatten()
+
+            # Compute `dot(vn, un)`.
+            dot_vn_un = 0
+            for key in un_flat:
+                dot_vn_un += torch.dot(vn_flat[key], un_flat[key])
+
+            # Initiate the history of vk_tas & dot_vk_uk_ta (recursion state in tff implementation)
+            vk_tas = defaultdict(list)
+            for key in vn_flat:
+                vk_tas[key].append(vn_flat[key])
+            dot_vk_uk_ta = [dot_vn_un]
+
+            # Iterate through all other samples, excluding samples 1 and 2
+            for n, sample in enumerate(samples[2:], 3):
+                # Update the running mean of the weights samples.
+                for key in weights_sample_mean:
+                    weights_sample_mean[key] = ((n - 1) * weights_sample_mean[key] + sample[key]) / n
+
+
+                # Compute u_{n} (deviation of the new sample from the previous mean).
+                un_flat = dict()
+                for key in sample:
+                    un_flat[key] = (sample[key] - weights_sample_mean[key]).flatten()
+
+                # Compute v_{n-1, n} (solution of `sigma_{n-1} x = u_n`).
+                # Step 1: compute `vk_coeff = gamma * dot(v_k, u_n) / (1 + gamma * uv_k)`.
+                gammas_range = 2 + torch.arange(0, n - 2)
+                gammas = rho * (gammas_range - 1) / gammas_range
+                dot_vk_un = dict()
+                for key in un_flat:
+                    dot_vk_un[key] = torch.einsum('ij,j->i', torch.stack(vk_tas[key]), un_flat[key])
+                dot_vk_un_tmp = 0
+                for key in dot_vk_un:
+                    dot_vk_un_tmp += torch.sum(dot_vk_un[key].flatten())
+                dot_vk_un = dot_vk_un_tmp
+                dot_vk_uk = torch.stack(dot_vk_uk_ta)
+                vk_coeffs = gammas * dot_vk_un / (1 + gammas * dot_vk_uk)
+
+                # Step 2: compute `vn = u - sum_k vk_coeff * vk` and `dot(v_n, u_n)`.
+                vn_flat = dict()
+                for key in un_flat:
+                    vn_flat[key] = un_flat[key] - torch.einsum('i,ij->j', vk_coeffs, torch.stack(vk_tas[key]))
+
+                # Compute `dot(vn, un)`.
+                dot_vn_un = 0
+                for key in sample:
+                    dot_vn_un += torch.dot(vn_flat[key], un_flat[key])
+
+                # Update the history of vk_tas & dot_vk_uk_ta (tff: recursion state)
+                for key in vk_tas:
+                    vk_tas[key].append(vn_flat[key])
+                dot_vk_uk_ta.append(dot_vn_un)
+
+                # Compute weights delta tilde: `weights_delta_tilde += coeff * vn / n`.
+                weight_delta_tilde_flat = dict()
+                dot_wd_un = 0
+                for key in weights_delta_tilde:
+                    weight_delta_tilde_flat[key] = weights_delta_tilde[key].flatten()
+                    dot_wd_un += torch.dot(weight_delta_tilde_flat[key], un_flat[key])
+                gamma = rho * (n - 1) / n
+                vn_coeff = 1. - gamma * (n * dot_wd_un + dot_vn_un) / (1. + gamma * dot_vn_un)
+                for key in weights_delta_tilde:
+                    weights_delta_tilde[key] = weights_delta_tilde[key] + vn_coeff * torch.reshape(vn_flat[key], weights_delta_tilde[key].shape) / n
+
+            # Obtain new weights delta by rescaling weights delta tilde.
+            weights_delta = dict()
+            for key in weights_delta_tilde:
+                weights_delta[key] = weights_delta_tilde[key] * (1 + (n - 1) * rho)
+
+            # Update from gradient to model weights.
+            new_weights = dict()
+            for key in weights_delta:
+                new_weights[key] = x_0[key].sub(weights_delta[key])
+            self.set_weights(new_weights)
 
     def _produce_iasg_sample(self, device):
         sample_weight = dict()
@@ -77,17 +135,7 @@ class FedPaClient(Base_Client):
                     sample_weight[key] = weight[key]
                 else:
                     sample_weight[key] = weight[key].add(sample_weight[key], alpha = i).div((i + 1))
-        return self._model_weight_to_array(sample_weight).to(device)
-
-
-    def _model_weight_to_array(self,w):
-        return torch.cat([w[k].flatten() for k in w]).detach()
-
-    def _array_to_model_weight(self,a):
-        a_tens = torch.split(a,self.layer_size)
-        model_w = {k:torch.reshape(a_tens[i],self.layer_shapes[i])
-                    for i,k in enumerate(self.layers )}
-        return model_w
+        return sample_weight
 
     def reset_optimizer(self):
         self.optimizer = optim.SGD(self.model.parameters(), lr = self.learning_rate,
