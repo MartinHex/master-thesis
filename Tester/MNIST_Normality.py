368d1045edeba938d595517e98e168e75f1d7ee3
Oscar Johansson <o.n.johansson@gmail.com>
2022-05-03 06:38:16 +0200

    Split MNIST into two seperate scripts

--- Tester/MNIST_Normality.py
+++ Tester/MNIST_Normality.py
@@ -2,8 +2,7 @@ import sys
 sys.path.append('.')
 from Models.MNIST_Model_lite import MNIST_Model as Model
 from Dataloaders.Mnist import Mnist as Dataloader
-from Algorithms.FedAvg import FedAvg
-from Algorithms.FedKp import FedKp
+from Algorithms.FedAvg import FedAvg as Alg
 from Models.Callbacks.Callbacks import Callbacks
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KernelDensity
@@ -30,7 +29,6 @@ alphas = [10,1,0.1,0.01]
 n_clients = 100
 seed = 0
 batch_size = 16
-n_runs = 5
 
 # Variables
 device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
@@ -43,19 +41,9 @@ print('Creating FedAvg')
 torch.manual_seed(seed)
 np.random.seed(seed)
 fedAvg = FedAvg(dl,Model,batch_size=batch_size,clients_per_round=10)
-print('Creating FedKp')
-torch.manual_seed(seed)
-np.random.seed(seed)
-fedKp = FedKp(dl,Model,batch_size=batch_size,cluster_mean=False,clients_per_round=10)
-torch.manual_seed(seed)
-np.random.seed(seed)
-fedKp_clstr = FedKp(dl,Model,batch_size=batch_size,cluster_mean=True,clients_per_round=10)
 
-alghs = {
-    'FedAvg':fedAvg,
-    'FedKp':fedKp,
-    'FedKP_cluter_mean':fedKp_clstr
-}
+# Initial Model
+torch.set
 
 # script
 def test_script(rounds,clients_per_rounds,epochs):
@@ -68,44 +56,31 @@ def test_script(rounds,clients_per_rounds,epochs):
     np.random.seed(seed)
     init_model = Model()
     cbs = Callbacks(test_data,verbose=False,device=device)
-    callbacks = [   cbs.server_thesis_results,
-                    cbs.server_training_thesis_results,]
-    alg_res = pd.DataFrame(columns=['algorithm','alpha','run',
-                                'train_loss','train_acc','train_rec','train_prec'
-                                'val_loss','val_acc','val_rec','val_prec','rounds','epochs','clients_per_round'])
+    callbacks = [  cbs.skew,cbs.kurtosis,cbs.ks_test,cbs.server_loss,cbs.server_accuracy]
+
+
+    dist_res = pd.DataFrame(columns=['algorithm','alpha','val_loss','val_acc'
+                                'ks_test','kurtosis','skew','rounds','epochs','clients_per_round'])
 
     for alpha in alphas:
-        print('alpha=%s'%str(alpha))
-        for run in range(n_runs):
-            dl = Dataloader(n_clients,alpha=alpha)
-            for alg in alghs:
-                alghs[alg].dataloaders = dl.get_training_dataloaders(batch_size)
-            for round,clients_per_round,epoch in zip(rounds,clients_per_rounds,epochs):
-                for alg in alghs:
-                    # Train algorithm
-                    alghs[alg].clients_per_round = clients_per_round
-                    alghs[alg].clients = [alghs[alg].client_generator(None,0) for _ in range(clients_per_round)]
-                    alghs[alg].server.model.set_weights(init_model.get_weights())
-                    alghs[alg].run(round,epochs=epoch,callbacks=callbacks,device=device,log_callbacks=False)
+        dl = Dataloader(n_clients,alpha=alpha)
+        for round,clients_per_round,epoch in zip(rounds,clients_per_rounds,epochs):
+            # Train algorithm
+            alg = ALG(dl,Model,batch_size=batch_size,clients_per_round=clients_per_round)
+            alg.server.model.set_weights(init_model.get_weights())
+            alg.run(round,epochs=epoch,callbacks=callbacks,device=device,log_callbacks=False)
 
-                    # Evaluate algorithm
-                    r = alghs[alg].get_callback_data()
-                    train_loss = np.mean(r['server_training_loss'][-1])
-                    train_acc = np.mean(r['server_training_accuracy'][-1])
-                    train_prec = r['server_training_precision'][-1]
-                    train_rec = r['server_training_recall'][-1]
-                    val_loss = r['server_loss'][-1]
-                    val_acc = r['server_accuracy'][-1]
-                    val_prec = r['server_precision'][-1]
-                    val_rec = r['server_recall'][-1]
+            # Evaluate algorithm
+            r = alg.get_callback_data()
+            val_loss = r['server_loss'][-1]
+            val_acc = r['server_accuracy'][-1]
 
-                    # Append to results
-                    alg_res = alg_res.append({'algorithm':alg,'alpha':alpha,'run':run,
-                        'train_loss':train_loss,'train_acc':train_acc,'train_prec':train_prec,'train_rec':train_rec,
-                        'val_loss':val_loss,'val_acc':val_acc,'val_prec':val_prec,'val_rec':val_rec,
-                        'rounds':round,'epochs':epoch,'clients_per_round':clients_per_round},ignore_index = True)
-
-    return alg_res
+            # Add distribution properties
+            for skew,kurtosis,ks in zip(r['skew'][-1],r['kurtosis'][-1],r['ks_test'][-1]):
+                dist_res = dist_res.append({'algorithm':alg,'alpha':alpha,'acc':val_acc,'loss':val_loss,
+                    'ks_test':ks,'kurtosis':kurtosis,'skew':skew,'rounds':round,
+                    'epochs':epoch,'clients_per_round':clients_per_round},ignore_index = True)
+    return dist_res
 
 
 
@@ -114,28 +89,26 @@ print('----------- Running test epoch test ------------------------')
 rounds = 1
 clients_per_rounds = 20
 epochs = [1,2,5,10]
-alg_res = test_script(rounds=rounds,clients_per_rounds=clients_per_rounds,epochs=epochs)
-alg_res.to_csv(os.path.join(log_path,'alg_res_epochs.csv'))
+dist_res = test_script(rounds=rounds,clients_per_rounds=clients_per_rounds,epochs=epochs)
+dist_res.to_csv(os.path.join(log_path,'dist_res_epochs.csv'))
 
 ################# Res 2 ###############################################
 print('----------- Running n_rounds epoch test ------------------------')
 rounds = [2,5,10,20]
 clients_per_rounds = 20
 epochs = 5
-alg_res = test_script(rounds=rounds,clients_per_rounds=clients_per_rounds,epochs=epochs)
-alg_res.to_csv(os.path.join(log_path,'alg_res_rounds.csv'))
+dist_res = test_script(rounds=rounds,clients_per_rounds=clients_per_rounds,epochs=epochs)
+dist_res.to_csv(os.path.join(log_path,'dist_res_rounds.csv'))
 
 ################# Res 3 ###############################################
 print('----------- Running n_clients epoch test ------------------------')
 rounds = 1
 clients_per_rounds = [5,10,50,100]
 epochs = 5
-alg_res = test_script(rounds=rounds,clients_per_rounds=clients_per_rounds,epochs=epochs)
-alg_res.to_csv(os.path.join(log_path,'alg_res_n_clients.csv'))
+dist_res = test_script(rounds=rounds,clients_per_rounds=clients_per_rounds,epochs=epochs)
+dist_res.to_csv(os.path.join(log_path,'dist_res_n_clients.csv'))
 
 #################### Plotting #####################################
-
-#################### Dists #################################
 metrics =['skew','kurtosis','ks_test']
 colors = sns.color_palette("ch:s=.25,rot=-.25", n_colors=4)
 
